be1115fe5314412991b139e892411e8832e26899
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2024-07-14 13:58:19 +0530

    Fix #20 : jenkins with docker installed inside container

--- jenkins/start_jenkins.sh
+++ jenkins/start_jenkins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-STARTING_PORT=${JENKINS_PORT:-9080}
+# STARTING_PORT=${JENKINS_PORT:-9080}
 LOG_FILE="jenkins_${STARTING_PORT}.log"
 > $LOG_FILE
 
 /usr/local/sbin/dropbear &
 
-java -version
-java -jar jenkins1.war $JAVA_OPTS --httpPort=$STARTING_PORT --logfile=$LOG_FILE &
+java -Djava.awt.headless=true -version
+java -Djava.awt.headless=true -jar jenkins1.war --httpPort=$JENKINS_PORT --logfile=$LOG_FILE &
 
 tail -f $LOG_FILE
